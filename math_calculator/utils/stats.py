893be135dc639dd90064699a600d769c08b41eb7
IreneKhymych <157988382+IreneKhymych@users.noreply.github.com>
2025-05-26 22:55:41 +0300

    Update stats.py

--- math_calculator/utils/stats.py
+++ math_calculator/utils/stats.py
@@ -2,10 +2,6 @@ import statistics
 
 def compute_statistics(nums):
     stats = {"середнє": statistics.mean(nums), "медіана": statistics.median(nums)}
-
-    try:
-        stats["мода"] = statistics.mode(nums)
-    except statistics.StatisticsError:
-        stats["мода"] = "немає моди"
-
+    modes = statistics.multimode(nums)
+    stats["мода"] = modes[0] if len(modes) == 1 else "немає моди"
     return stats
